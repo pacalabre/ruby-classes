7ed4b5db3ff4bd89846b1e5121203c90ca8e31d3
Paul <pacalabre@gmail.com>
2016-05-02 16:04:33 -0700

    update dice

--- Dice.rb
+++ Dice.rb
@@ -1,15 +1,18 @@
-
 class Dice
-
 
   def number_of_sides
     6
   end
 
   def roll
-    1 +  rand(number_of_sides)
+   1 +  rand(number_of_sides)
+  end
+
+  def get_rolls
+    []
   end
 end
+
 
 
 dice = Dice.new
